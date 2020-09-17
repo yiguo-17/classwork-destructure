281ff20b6d6ecd852e41422aaf6720ddc4f46dac
gingu-code <48529532+gingu-code@users.noreply.github.com>
2020-09-17 12:03:55 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+/*You will probably not have to take destructuring to such a level as this but this is for you to really wrap your brains around the mechanics.
+EVERY EXERCISE SHOULD USE DESTRUCTURING*/
+
+
+1. let arr = [
+   ['name', 'jon'],
+   ['age', 20],
+   ['eyes', 'blue'],
+   ];
+ //Create an object out of the arr above then using destructuring, declare variables using the object keys.
+ 
+ 
+2. 
+let arr = [1,2,3,4,5,6]
+  //a. Set 3 variables names: the third index should be called 'three', the fourth item 'four' and the rest of the items thereafter 'rest'
+  //b. Swap the third and fourth item variable values
